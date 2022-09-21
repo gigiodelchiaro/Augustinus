f68552252c17122d43f170922d04b5a89533048e
Giovanni Del Chiaro <del.chiaro@hotmail.com>
2022-09-20 22:31:22 -0300

    no gui beta 1

---
+++ sillabes.py
@@ -0,0 +1,26 @@
+#• As vogais que representam hiatos:
+#
+#ci-ú-me, du-e-lo, lu-a, ru-í-do, vi-ú-va...
+#
+#• As letras que indicam os dígrafos (“rr”, “ss”, “sc”, “sç” e “xc”):
+#
+#bar-ri-ga, tra-ves-sa, dis-ci-pli-na, nas-ço, ex-ce-len-te...
+#• Os encontros consonantais que são “separáveis”, levando-se em consideração a regra da soletração. Eles aparecem no interior dos vocábulos:
+#
+#ab-sur-do, ad-vér-bio, af-to-so, cor-rup-ção, téc-ni-ca...
+#
+#O que não podemos separar?
+#• Letras que formam ditongos e tritongos:
+#
+#
+#á-gua, di-nhei-ro, i-dei-a, joi-a, jau-la...
+#
+#a-ve-ri-guou, ex-tor-quiu, quais, sa-guão, Uruguai...
+#
+#• As letras que representam os dígrafos (“ch”, “lh”, “nh”, “gu” e “qu”):
+#
+#cha-ve, mi-lho, ni-nho, gui-a, que-ri-do...
+#
+#• Os encontros consonantais que são “inseparáveis”, tendo em vista a regra da soletração. Normalmente, esse tipo de encontrado consonantal é composto de uma consoante mais “l” ou “r”:
+#
+#cla-re-za, Bra-sil, em-pre-sa, pa-les-tra, psi-có-lo-go...
