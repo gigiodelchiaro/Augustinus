608a86afc982d93682f9fb3b993d36bec7baa285
Giovanni Del Chiaro <69433647+gigiodelchiaro@users.noreply.github.com>
2023-01-11 10:47:34 -0300

    Beta salvar espaços

--- sillabes.py
+++ sillabes.py
@@ -12,27 +12,18 @@
 #
 #á-gua, di-nhei-ro, i-dei-a, joi-a, jau-la...
 #
-#a-ve-ri-guou, ex-tor-quiu, quais, sa-guão, Uruguai...
-#
-#• Os encontros consonantais que são “inseparáveis”, tendo em vista a regra da soletração. Normalmente, esse tipo de encontrado consonantal é composto de uma consoante mais “l” ou “r”:
-#
-#cla-re-za, Bra-sil, em-pre-sa, pa-les-tra, psi-có-lo-go...
-
-
-
-text = input("Texto a separar: ")
-text = text.replace("rr","r-r")
-text = text.replace("ss","s-s")
-text = text.replace("sc","s-c")
-text = text.replace("sç","s-ç")
-text = text.replace("xc","x-c")
-text = text.replace("ch","-ch")
-text = text.replace("lh","-lh")
-text = text.replace("nh","-nh")
-text = text.replace("gu","-gu")
-text = text.replace("qu","-qu")
-text = text.replace(" -"," ")
-text = text.replace("- "," ")
-text = text.replace("--","-")
-print(text)
-input("")
+#a-ve-ri-guou, ex-tor-quiu, quais, sa-guão, 
+spaces []
+count = 0
+While true:
+    # char = get text(pos = count)
+    if char = " ":
+        spaces.append(count)
+    elif char != " ":
+        count = count + 1
+    elif count = 100:
+        break
+    else:
+        break
+    
+    
