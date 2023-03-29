4dd1d0764c0649c7e48e6b52f42f6b2b8809042e
Giovanni Del Chiaro <69433647+gigiodelchiaro@users.noreply.github.com>
2023-03-29 07:19:27 -0300

    capital letters

--- ai.py
+++ ai.py
@@ -1,62 +1,62 @@
 
 if __name__ == '__main__':
-    import sys
-    if len(sys.argv) < 5:
-        print("Usage: text, clef, note, line break, respiration")
-        exit(1)
-    inputText = sys.argv[1]
-    clef = sys.argv[2]
-    note = sys.argv[3].casefold()
-    lnBreak = bool(sys.argv[4])
-    resp = bool(sys.argv[5])
+	import sys
+	'''
+	try:
+		inputText = sys.argv[1]
+		clef = sys.argv[2]
+		note = sys.argv[3].casefold()
+		lnBreak = bool(sys.argv[4])
+		resp = bool(sys.argv[5])
 
-
+	except IndexError:
+		print("Usage: text, clef, note, line break, respiration")
+		sys.exit(0)
+	'''
+	inputText = "Senhor nosso Deus, dai-nos, por vossa graça, caminhar com alegria na mesma caridade que levou o vosso Filho a entregar-se à morte no seu amor pelo mundo. Por nosso Senhor Jesus Cristo, vosso Filho, na unidade do Espírito Santo."
+	clef = 'c4' 
+	note = 'f' 
+	lnBreak = 'y' 
+	resp = 'y'
 def syllable(text):
 	import os
-	import re
+	
 	lpalavra = text.split()
 	where = []
-
+	separated = []
 
 	os.environ['CLASSPATH'] = "./sources/fb_nlplib.jar"
-
 	from jnius import autoclass
-
 	class FalaBrasilNLP:
 		def __init__(self):
 			self.jClass = autoclass('ufpa.util.PyUse')()
 			self.fb_getsyl = self.jClass.useSyll
-
-		def fb_process_input(self, text):
-			lpalavra = text.split()
-			separated = [self.fb_getsyl(i.replace(".", "").replace(",", "")) for i in lpalavra]
-			return separated
-	if __name__ == '__main__':
-		fb_nlp = FalaBrasilNLP()
-		#where = [".,".find(i) if i in ".," else "" for i in lpalavra]
-
-		for i in lpalavra:
-			if re.search(r"\.", i):
-				where.append(".")
     
-			elif re.search(r",", i):
-				where.append(",")
-    
-			else:
-				where.append("")
-   
-		i = i.replace(".", "")
-		i = i.replace(",", "")
-		separated = [fb_nlp.fb_getsyl(i.replace(".", "").replace(",", "")) for i in lpalavra]
+	fb_nlp = FalaBrasilNLP()
+
+	for i in lpalavra:
+		where.append(i[-1]) if i[-1] == ',' or i[-1] == '.' else where.append('')
+  
+		if i[0].isupper():
+			hasUpper = True
+		else:
+			hasUpper = False
+		if hasUpper:
+			separated.append(fb_nlp.fb_getsyl(i.replace(".", "").replace(",", "").casefold()).capitalize())
+		else:
+			separated.append(fb_nlp.fb_getsyl(i.replace(".", "").replace(",", "").casefold()))
+		# separated = [i.replace(".", "").replace(",", "").casefold()]
+
+
+
 		
 
+				
+	
 
-		final = list(map(lambda s, w: s + w, separated, where))
-  
-  
-noteList = ['a','b','c','d','e','f','g','h','i','j','k','l','m']
-note_down = noteList[noteList.index(note) - 1] if note != 'a' else 'a'
-text = f'({clef}){inputText} '
+	return " ".join(list(map(lambda s, w: s + w, separated, where)))
+
+text = f'({clef}){syllable(inputText)} '
 
 toReplace = {
     '-': f'-({note})',
