a0c048e6b22e356c1b6e2a9d9e31144b827d831e
gigiodelchiaro <gigiodc@proton.me>
2025-01-15 00:33:10 -0300

    modelo (1 linha)

--- docs/gabc.js
+++ docs/gabc.js
@@ -1,50 +1,30 @@
- /* testing exsurge */
- var ctxt = new exsurge.ChantContext();
- ctxt.lyricTextFont = "'Crimson Text', serif";
- ctxt.lyricTextSize *= 1.2;
- ctxt.dropCapTextFont = ctxt.lyricTextFont;
- ctxt.annotationTextFont = ctxt.lyricTextFont;
- var score;
- var gabcSource = document.getElementById('gabcSource');
- var chantContainer = document.getElementById('chant-container');
- //
- // to use canvas drawing, you should use the canvas object belonging to the
- // canvas, resizing it as below. The reason for custom resizing is that the
- // canvas drawing takes into consideration screen dpi in order to render
- // the highest possibly quality on lots of different screens.
- // 
- // 
- // document.querySelector('body').appendChild(ctxt.canvas);
- // ctxt.setCanvasSize(1280, 720);
- //
- // To render to the canvas, you can use a standard animation loop, which
- // draws to the canvas at the desired intervals, e.g.:
- // 
- //
- // function animloop() {
- //   requestAnimationFrame(animloop);
- //
- //   if (score)
- //     score.draw(ctxt);
- // };
- //
- var updateChant = function() {
-   if (score) {
-     exsurge.Gabc.updateMappingsFromSource(ctxt, score.mappings, gabcSource.value);
-     score.updateNotations(ctxt);
-   } else {
-     mappings = exsurge.Gabc.createMappingsFromSource(ctxt, gabcSource.value);
-     score = new exsurge.ChantScore(ctxt, mappings, true);
-     score.annotation = new exsurge.Annotation(ctxt, "%V%");
-   }
-   layoutChant();
- }
- var layoutChant = function() {
-   // perform layout on the chant
-   score.performLayoutAsync(ctxt, function() {
-     score.layoutChantLines(ctxt, chantContainer.clientWidth, function() {
-       // render the score to svg code
-       chantContainer.innerHTML = score.createSvg(ctxt);
-     });
-   });
- }
+/* testing exsurge */
+var ctxt = new exsurge.ChantContext();
+ctxt.lyricTextFont = "'Crimson Text', serif";
+ctxt.lyricTextSize *= 1.2;
+ctxt.dropCapTextFont = ctxt.lyricTextFont;
+ctxt.annotationTextFont = ctxt.lyricTextFont;
+var score;
+var gabcSource = document.getElementById('gabcSource');
+var chantContainer = document.getElementById('chant-container');
+
+var updateChant = function () {
+	if (score) {
+		exsurge.Gabc.updateMappingsFromSource(ctxt, score.mappings, gabcSource.value);
+		score.updateNotations(ctxt);
+	} else {
+		mappings = exsurge.Gabc.createMappingsFromSource(ctxt, gabcSource.value);
+		score = new exsurge.ChantScore(ctxt, mappings, true);
+		score.annotation = new exsurge.Annotation(ctxt, "%V%");
+	}
+	layoutChant();
+}
+var layoutChant = function () {
+	// perform layout on the chant
+	score.performLayoutAsync(ctxt, function () {
+		score.layoutChantLines(ctxt, chantContainer.clientWidth, function () {
+			// render the score to svg code
+			chantContainer.innerHTML = score.createSvg(ctxt);
+		});
+	});
+}
--- docs/script.js
+++ docs/script.js
@@ -2,9 +2,12 @@
 var clefs = ["c1", "c2", "c3", "c4", "f2", "f3", "f4", "cb3"];
 var notes = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"];
 
-
+function note_merge(string1, string2)
+{
+    return string1.replace(")", "") + string2.replace("(", "");
+}
 function processText() {
-    const separator = document.getElementById('separator').value;
+    const separator = "@";
     definirSeparador(separator);
 
     const textInput = document.getElementById('text').value;
@@ -18,12 +21,10 @@ function processText() {
     
     let text = textInput;
     
-    const clef = clefs.find(clef => new RegExp(`\\b${clef}\\b`).test(model));
-    const note_pattern = `\\([${notes.join("")}]+(r)?(r1)?\\)`;
-
-    const all_notes = model.match(new RegExp(`${note_pattern}`, "g"));
+    const all_symbols = model.match(/\(\S+\)/gm);
     
     let final = ""
+    
     if (removeNumbers) {
         text = text.replace(/\d/gm, '');
     }
@@ -33,25 +34,122 @@ function processText() {
         for (let word of words) {
             let syllables = separarTexto(word).split(separator);
             let tonicNumber = tonica(syllables);
-            tonicNumber = syllables.length - tonicNumber ;
+            tonicNumber = syllables.length - tonicNumber;
             syllables[tonicNumber] = "<b>" + syllables[tonicNumber] + "</b>";
-            
             result += syllables.join(separator)
             if (addEnd) {
                 result += separator;
             }
             result += " ";
+
         }
-        final = result;;
+        final = result;
     }
     else {
-        const processedText = separarTexto(text);
+        const processedText = separarTexto(text).replaceAll(" ", separator + " ");
         final = processedText
     }
+    let all_syllables = final.split(separator);
+    const note_pattern = `\\([${notes.join("")}]+\\)`;
+    const tonic_pattern = `\\([${notes.join("")}]+r1\\)`;
+    const generic_note_pattern = `\\([${notes.join("")}]+r\\)`;
+    let gabc = all_symbols[0] + " ";
+    let remaining_syllables = all_syllables;
+    let hit_generic_note = false;
+    let hit_last_note = false;
+    let words = final.split(" ");
+    let tonic_last_note = tonica(words[words.length - 1].split(separator));
+    let generic_note = "";
+    let remaining_symbols = all_symbols;
+    let notes_before_tonic = [];
+    remaining_symbols.shift();
+    while (remaining_symbols.length > 0) {
+        let symbol = all_symbols[0];
+        if (symbol.match(note_pattern))
+        {
+            if (hit_generic_note){
+                notes_before_tonic.push(symbol);
+            }
+            else{
+                gabc += remaining_syllables.shift() + symbol;
+            }
+            remaining_symbols.shift();
+        }
+        else if (symbol.match(generic_note_pattern))
+        {
+            if(!hit_generic_note){
+                generic_note = symbol;
+                gabc += symbol;
+                hit_generic_note = true;
+            }
+            remaining_symbols.shift();
+        }
+        else if (symbol.match(tonic_pattern))
+        {
+            remaining_symbols.shift();
+            let last_symbols = remaining_symbols.join(" ");
+            symbol = symbol.replace("r1", "");
+            gabc += remaining_syllables[remaining_syllables.length - tonic_last_note] + symbol;
+            let middle_note = last_symbols.match(generic_note_pattern)[0].replace("r", "");
+            last_symbols = last_symbols.replace(generic_note_pattern, "");
+            last_note = last_symbols.match(note_pattern)[0];
+
+            
+            if (tonic_last_note == 1){
+
+                
+                gabc = gabc.slice(0, -1);
+                gabc += last_note.replace("(", "");
+            }
+            else if (tonic_last_note == 2){
+
+                gabc += remaining_syllables[remaining_syllables.length - tonic_last_note + 1] + last_note;
+                remaining_syllables.pop();
+            }
+            else {
+                let syllables_to_add = remaining_syllables.slice(tonic_last_note*(-1) + 1);
+                for (let i = 0; i < syllables_to_add.length; i++) {
+                    gabc += syllables_to_add.shift() + middle_note;
+                }
+                gabc += syllables_to_add.shift() + last_note;
+            }
+            remaining_symbols.shift();
+            remaining_symbols.shift();
+            for (let index = 0; index < tonic_last_note; index++) {
+                remaining_syllables.pop();
+            }
+            last_symbols = remaining_symbols.join(" ");
+            gabc += last_symbols;
+            hit_last_note = true;
+            break;
+        }
+        else if (!hit_last_note){
+            gabc += symbol;
+            remaining_symbols.shift();
+        }
+        else {
+            remaining_symbols.shift();
+        }
+        
+    }
+    note_to_add = generic_note.replace("r", "");
+    while (remaining_syllables.length > notes_before_tonic.length) {
+        let index = gabc.match(generic_note).index - 1;
+        gabc = gabc.slice(0, index) + remaining_syllables.shift() + note_to_add + gabc.slice(index);
+    }
+
+    while (remaining_syllables.length > 0) {
+        let note = notes_before_tonic.shift();
+        if (note == undefined)
+        {
+            note = note_to_add;
+        }
+        let index = gabc.match(generic_note).index - 1;
+        gabc = gabc.slice(0, index) + remaining_syllables.shift() + note + gabc.slice(index);
+    }
+    gabc = gabc.replace(generic_note, "");
     resultOutput.innerHTML = final;
-    console.log(clef);
-    console.log(all_notes);
-    gabcSource.value = model;
+    gabcSource.value = gabc;
     updateChant()
 }
 
