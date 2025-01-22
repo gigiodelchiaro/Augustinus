ee0e7b77f76d735aeae4828b093d0f98a1abc0de
gigiodelchiaro <gigiodc@proton.me>
2025-01-22 17:48:07 -0300

    modo oração

--- docs/script.js
+++ docs/script.js
@@ -1,133 +1,162 @@
+// Get the checkbox and advanced elements
+const advancedCheckbox = document.getElementById('advanced');
+const advancedElements = document.querySelectorAll('.advanced');
 
-function setModel() {
+// Add an event listener to the checkbox
+advancedCheckbox.addEventListener('change', function() {
+    // Loop through all advanced elements
+    advancedElements.forEach(element => {
+        // Toggle visibility based on checkbox state
+        element.style.display = this.checked ? 'block' : 'none';
+    });
+});
+
+function updateModelDisplay() {
     document.getElementById('model').innerHTML = document.getElementById("models").value;
     initializeAndLayoutChant("model", "svg-model");
 }
+function generateGabcNotation() {
+    const syllableSeparator = "@";
+    definirSeparador(syllableSeparator);
 
+    const inputText = document.getElementById('text').value;
+    const shouldRemoveNumbers = document.getElementById('remove-numbers').checked;
+    const gabcOutputElement = document.getElementById('gabc');
+    const flexaTemplateElement = document.getElementById('flexa');
+    const selectedModelPattern = document.getElementById('model').value;
 
-function processText() {
+    const modelSymbols = selectedModelPattern.match(/\([^\(\)]+\)/gm);
+    const basicNoteRegex = /[a-m][^\dr]/;
+    const tonicNoteRegex = /\([^\s]+r1\)/;
+    const genericNoteRegex = /\(([a-z]r\s?)+\)/;
 
-    const separator = "@";
-    definirSeparador(separator);
-
-    const textInput = document.getElementById('text').value;
-    const removeNumbers = document.getElementById('remove-numbers').checked;
-    const gabcSource = document.getElementById('gabc');
-
-    const model = document.getElementById('model').value;
-    const all_symbols = model.match(/\([^\(\)]+\)/gm);
-    const note_pattern = /[a-m][^\dr]/;
-    const tonic_pattern = /\([^\s]+r1\)/;
-    const generic_note_pattern = /\(([a-z]r\s?)+\)/;
-    const fragments = textInput.split("\n");
-
-    
-
-    let gabc = "";
-    for (let f = 0; f < fragments.length; f++) {
-        let text = fragments[f];
-        console.log(text);
-        let final = ""
-
-        if (removeNumbers) {
-            text = text.replace(/\d/gm, '');
-        }
-
-        const processedText = separarTexto(text).replaceAll(" ", separator + " ");
-        final = processedText
-
-        let all_syllables = final.split(separator);
-
-        
-        let remaining_syllables = all_syllables;
-
-        let hit_generic_note = false;
-        let hit_last_note = false;
-
-        let words = final.split(" ");
-        let tonic_last_note = tonica(words[words.length - 1].split(separator));
-
-        let generic_note = "";
-        let remaining_symbols = all_symbols;
-        let notes_before_tonic = [];
-
-        while (remaining_symbols.length > 0) {
-            let symbol = remaining_symbols[0];
-
-            if (symbol.match(note_pattern)) {
-                if (hit_generic_note) {
-                    notes_before_tonic.push(symbol);
-                }
-                else {
-                    gabc += remaining_syllables.shift() + symbol;
-                }
-                remaining_symbols.shift();
-            }
-            else if (symbol.match(generic_note_pattern)) {
-                if (!hit_generic_note) {
-                    generic_note = "(" + symbol.match(/[a-m]/) + ")";
-                    gabc += symbol;
-                    hit_generic_note = true;
-                }
-                remaining_symbols.shift();
-            }
-            else if (symbol.match(tonic_pattern)) {
-                remaining_symbols.shift();
-                let last_symbols = remaining_symbols.join(" ");
-                symbol = symbol.replace("r1", "");
-                gabc += remaining_syllables[remaining_syllables.length - tonic_last_note] + symbol;
-                let middle_note = last_symbols.match(generic_note_pattern)[0].replace("r", "");
-                last_symbols = last_symbols.replace(generic_note_pattern, "");
-                last_note = "(" + last_symbols.match(note_pattern)[0];
-
-
-                if (tonic_last_note == 1) {
-
-                    gabc = gabc.slice(0, -1);
-                    gabc += last_note.replace("(", "");
-                }
-
-                else {
-                    let syllables_to_add = remaining_syllables.slice(tonic_last_note * (-1) + 1);
-                    for (let i = 1; i < syllables_to_add.length; i++) {
-                        gabc += syllables_to_add.shift() + middle_note;
-                    }
-                    gabc += syllables_to_add.shift() + last_note;
-                }
-                remaining_symbols.shift();
-                remaining_symbols.shift();
-                for (let index = 0; index < tonic_last_note; index++) {
-                    remaining_syllables.pop();
-                }
-                last_symbols = remaining_symbols.join(" ");
-                //gabc += last_symbols;
-                hit_last_note = true;
+    const textFragments = inputText.split("\n");
+    let gabcOutput = "";
+    const endings = [
+        "Por nosso Senhor Jesus Cristo, vosso Filho, que é Deus, e convosco vive e reina, na unidade do Espírito Santo, por todos os séculos dos séculos.",
+        "Por(g) nos(h)so(h) Se(h)nhor(h) Je(h)sus(h) Cris(h)to,(h) vos(h)so(h) Fi(h)lho,(h) que(h) é(h) Deus,(hg) (:) e(g) con(h)vos(h)co(h) vi(h)ve(h) e(h) rei(h)na,(h) na(h) u(h)ni(h)da(h)de(h) do(h) Es(h)pí(g)ri(g)to(h) San(h)to,(h) (:) por(g) to(h)dos(h) os(h) sé(h)cu(h)los(g) dos(h) sé(h)cu(g)los.(g) (::)",
+        "Por Cristo nosso Senhor.",
+        "Por(g) Cris(h)to,(h) nos(h)so(g) Se(h)nhor.(hg) (::)",
+        "Vós que sois Deus, e vós que viveis e reinais com o Pai, na unidade do Espírito Santo, por todos os séculos dos séculos.",
+        "Vós(g) que(h) sois(h) Deus,(h) e(h) vi(h)veis(h) e(h) e(h) rei(h)nais(h) com(h) o(h) Pai,(hg) (:) na(g) u(h)ni(h)da(h)de(h) do(h) Es(h)pí(g)ri(g)to(h) San(h)to,(h) (:) por(g) to(h)dos(h) os(h) sé(h)cu(h)los(g) dos(h) sé(h)cu(g)los.(g) (::)",
+        "Ele que é Deus, e conosco vive e reina na unidade do Espírito Santo, por todos os séculos dos séculos.",
+        "E(g)le(h) que_é(h) Deus,(h) e(h) con(h)vos(h)co(h) vi(h)ve(h) e(h) rei(h)na,(g) (:) na(g) u(h)ni(h)da(h)de(h) do(h) Es(h)pí(g)ri(g)to(h) San(h)to,(h) (:) por(g) to(h)dos(h) os(h) sé(h)cu(h)los(g) dos(h) sé(h)cu(g)los.(g) (::)",
+        "Ele que vive e reina pelos séculos dos séculos.",
+        "E(g)le(h) que(h) vi(h)ve(h) e(h) rei(h)na(h) pe(h)los(h) sé(h)cu(h)los(g) dos(h) sé(h)cu(g)los(g) (::)"
+    ];
+    for (let fragmentIndex = 0; fragmentIndex < textFragments.length; fragmentIndex++) {
+        let isEnding = false;
+        let currentText = textFragments[fragmentIndex];
+        for (let endingIndex = 0; endingIndex < endings.length / 2; endingIndex++) {
+            if (currentText.toLowerCase().replaceAll(/\W+/gm, '') == endings[endingIndex].toLowerCase().replaceAll(/\W+/gm, '')) {
+                gabcOutput += endings[endingIndex + 1];
+                isEnding = true;
                 break;
             }
-            else if (!hit_last_note) {
-                gabc += symbol;
-                remaining_symbols.shift();
-            }
-            else {
-                remaining_symbols.shift();
-            }
+        }
+        if (isEnding) {
+            continue;
+        }
+        let processedTextFinal = "";
 
-        } 
-
-        while (remaining_syllables.length > notes_before_tonic.length) {
-            let index = gabc.match(generic_note_pattern).index;
-            gabc = gabc.slice(0, index) + remaining_syllables.shift() + generic_note + gabc.slice(index);
+        if (shouldRemoveNumbers) {
+            currentText = currentText.replace(/\d/gm, '');
         }
 
-        while (remaining_syllables.length > 0) {
-            let index = gabc.match(generic_note_pattern).index;
-            gabc = gabc.slice(0, index) + remaining_syllables.shift() + notes_before_tonic.shift() + gabc.slice(index);
+        const processedText = separarTexto(currentText).replaceAll(" ", syllableSeparator + " ");
+        processedTextFinal = processedText;
+
+        let syllablesList = processedTextFinal.split(syllableSeparator);
+        let remainingSymbols = modelSymbols;
+        let remainingSyllables = syllablesList;
+        if (remainingSyllables[remainingSyllables.length - 1] == " +") {
+            remainingSyllables.pop();
+            remainingSymbols = flexaTemplateElement.value.match(/\([^\(\)]+\)/gm);
         }
-        gabc = gabc.replace(generic_note_pattern, "");
-        gabc = gabc.replace(generic_note_pattern, "");
+
+        let hasGenericNote = false;
+        let hasLastNote = false;
+
+        let wordsArray = processedTextFinal.replace(" +", "").split(" ");
+        let tonicSyllablePosition = tonica(wordsArray[wordsArray.length - 1].split(syllableSeparator));
+        let genericNotePlaceholder = "";
+        let preTonicNotes = [];
+
+        while (remainingSymbols.length > 0) {
+            let currentSymbol = remainingSymbols[0];
+
+            if (currentSymbol.match(basicNoteRegex)) {
+                if (hasGenericNote) {
+                    preTonicNotes.push(currentSymbol);
+                } else {
+                    gabcOutput += remainingSyllables.shift() + currentSymbol;
+                }
+                remainingSymbols.shift();
+            }
+            else if (currentSymbol.match(genericNoteRegex)) {
+                if (!hasGenericNote) {
+                    genericNotePlaceholder = "(" + currentSymbol.match(/[a-m]/) + ")";
+                    gabcOutput += currentSymbol;
+                    hasGenericNote = true;
+                }
+                remainingSymbols.shift();
+            }
+            else if (currentSymbol.match(tonicNoteRegex)) {
+                remainingSymbols.shift();
+                let remainingSymbolsString = remainingSymbols.join(" ");
+                currentSymbol = currentSymbol.replace("r1", "");
+
+                gabcOutput += remainingSyllables[remainingSyllables.length - tonicSyllablePosition] + currentSymbol;
+
+                let middleNoteSymbol = remainingSymbolsString.match(genericNoteRegex)[0].replace("r", "");
+                remainingSymbolsString = remainingSymbolsString.replace(genericNoteRegex, "");
+                let finalNoteSymbol = "(" + remainingSymbolsString.match(basicNoteRegex)[0];
+
+                if (tonicSyllablePosition == 1) {
+                    gabcOutput = gabcOutput.slice(0, -1);
+                    gabcOutput += finalNoteSymbol.replace("(", "");
+                } else {
+                    let syllablesToInsert = remainingSyllables.slice(tonicSyllablePosition * (-1) + 1);
+                    for (let i = 1; i < syllablesToInsert.length; i++) {
+                        gabcOutput += syllablesToInsert.shift() + middleNoteSymbol;
+                    }
+                    gabcOutput += syllablesToInsert.shift() + finalNoteSymbol;
+                }
+
+                remainingSymbols.shift();
+                remainingSymbols.shift();
+                for (let index = 0; index < tonicSyllablePosition; index++) {
+                    remainingSyllables.pop();
+                }
+                remainingSymbolsString = remainingSymbols.join(" ");
+                gabcOutput += " " + remainingSymbolsString;
+                hasLastNote = true;
+                break;
+            }
+            else if (!hasLastNote) {
+                gabcOutput += currentSymbol;
+                remainingSymbols.shift();
+            } else {
+                remainingSymbols.shift();
+            }
+        }
+
+        while (remainingSyllables.length > preTonicNotes.length) {
+            let placeholderIndex = gabcOutput.match(genericNoteRegex).index;
+            gabcOutput = gabcOutput.slice(0, placeholderIndex) + remainingSyllables.shift() + genericNotePlaceholder + gabcOutput.slice(placeholderIndex);
+        }
+
+        while (remainingSyllables.length > 0) {
+            let placeholderIndex = gabcOutput.match(genericNoteRegex).index;
+            gabcOutput = gabcOutput.slice(0, placeholderIndex) + remainingSyllables.shift() + preTonicNotes.shift() + gabcOutput.slice(placeholderIndex);
+        }
+
+        gabcOutput = gabcOutput.replace(genericNoteRegex, "");
+        gabcOutput = gabcOutput.replace(genericNoteRegex, "");
     }
-    gabcSource.value = gabc;
+
+    gabcOutputElement.value = gabcOutput;
     initializeAndLayoutChant("gabc", "svg-final");
     initializeAndLayoutChant("model", "svg-model");
+    initializeAndLayoutChant("flexa", "svg-flexa");
 }
-
